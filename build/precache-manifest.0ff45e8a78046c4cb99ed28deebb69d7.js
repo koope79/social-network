d9292d83fd5d63e212a1d76403472a26e0e5cb4a
koope79 <70471003+koope79@users.noreply.github.com>
2021-10-22 21:11:36 +0300

    build

---
+++ build/precache-manifest.0ff45e8a78046c4cb99ed28deebb69d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba18d86d1ea1674098d4a7eadad81bfe",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "a7676e7ed15114b88f6f",
+    "url": "/social-network/static/css/3.eead8743.chunk.css"
+  },
+  {
+    "revision": "788b8d3500456b9eac25",
+    "url": "/social-network/static/css/4.5de11ea3.chunk.css"
+  },
+  {
+    "revision": "f75eee85bffe4842760f",
+    "url": "/social-network/static/css/main.7ab0b74e.chunk.css"
+  },
+  {
+    "revision": "6e0633859c401a8ffc59",
+    "url": "/social-network/static/js/2.c656d989.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/social-network/static/js/2.c656d989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7676e7ed15114b88f6f",
+    "url": "/social-network/static/js/3.2b55c74e.chunk.js"
+  },
+  {
+    "revision": "788b8d3500456b9eac25",
+    "url": "/social-network/static/js/4.55f94141.chunk.js"
+  },
+  {
+    "revision": "f75eee85bffe4842760f",
+    "url": "/social-network/static/js/main.68c8ad2a.chunk.js"
+  },
+  {
+    "revision": "d812f3b3c50a1ea39e20",
+    "url": "/social-network/static/js/runtime-main.094eec89.js"
+  },
+  {
+    "revision": "244050348438017b07e6debdb16e8075",
+    "url": "/social-network/static/media/preloader.24405034.gif"
+  }
+]);
